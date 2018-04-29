739f10f8d7ec1b279f3daf0457777bb48e2c3fca
Anton <Anton_fast@hotmail.com>
2018-04-29 15:39:10 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,6 +89,7 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+					<a href="{{ url('/test') }}">Test</a>					
                 </div>
             </div>
         </div>
