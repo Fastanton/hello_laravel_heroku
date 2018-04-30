74bb93ce2a34444fce3b9d85c35745e893795251
Anton <Anton_fast@hotmail.com>
2018-05-01 01:05:44 +0200

    make

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -68,13 +68,13 @@
 			</ul>
 		</div>
 		<div class="col-md-9 pull-md-3" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin:10px 0px 10px 0px">
-			<div><h2 style="text-align: center;">Client</h2></div>
+			<div><h2 style="text-align: center;">Client AB</h2></div>
 			<div class="progress">
 			  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
 				<span class="sr-only">60% Complete</span>
 			  </div>
 			</div>
-			<iframe src="https://calendar.google.com/calendar/embed?src=info%40fastwebbproduktion.se&ctz=Europe%2FStockholm" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+			<iframe src="https://calendar.google.com/calendar/embed?src=info%40fastwebbproduktion.se&ctz=Europe%2FStockholm" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
 			<hr>
 			<div class="row">
 				<div class="col-6" style="width: 49%;margin: 0;float: left;margin: 5px;border:1px solid #e2e2e2;border-radius:3px;">
