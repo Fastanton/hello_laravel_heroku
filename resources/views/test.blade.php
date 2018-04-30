efc3241c38ad875efb70539123ec5c1a57059752
Anton <Anton_fast@hotmail.com>
2018-05-01 00:25:41 +0200

    make it better

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -58,7 +58,7 @@
 </nav>
 	</header>
 	<div>
-		<div class="col-md-3 push-md-9" style="box-shadow: 5px 10px;">
+		<div class="col-md-3 push-md-9" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 			
 			<ul class="nav nav-pills nav-stacked">
 				<li><a href="#">Test</a></li>
@@ -66,7 +66,7 @@
 				<li><a href="#">Test</a></li>				
 			</ul>
 		</div>
-		<div class="col-md-9 pull-md-3" style="box-shadow: 5px 10px;">
+		<div class="col-md-9 pull-md-3" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 			<div>Client</div>
 			<div class="progress">
 			  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
