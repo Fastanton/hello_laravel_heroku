5cef915755cd7c679f225e26532f4a8b5640d221
Anton <Anton_fast@hotmail.com>
2018-04-30 23:05:26 +0200

    make it better

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,7 +9,11 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" type="text/css" href="/css/app.css">		
+		<link rel="stylesheet" type="text/css" href="/css/app.css">	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>			
 
     </head>
     <body>
@@ -70,10 +74,6 @@
 	</header>
         <div class="flex-center position-ref full-height">
 
-        </div>
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>		
+        </div>	
     </body>
 </html>
