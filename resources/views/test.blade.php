c1ce0677dcc6347cbc78f0f1f7593471fc6dc89b
Anton <Anton_fast@hotmail.com>
2018-05-01 22:28:32 +0200

    make

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -34,8 +34,6 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li role="presentation" class="active"><a href="#">Dashboard</a></li>
-
       </ul>
       <ul class="nav navbar-nav navbar-right">
 <ul class="nav nav-pills" role="tablist">
