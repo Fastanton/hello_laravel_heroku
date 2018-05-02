1d2768aee57475d6473bc3393801c105ed693b84
Anton <Anton_fast@hotmail.com>
2018-05-02 20:52:37 +0200

    make

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -50,8 +50,15 @@
 </nav>
 	</header>
 	<div>
-		@section('sidebar')
-		@show
+		<div class="col-md-3 push-md-9" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);width:20%;margin: 10px 20px 10px 20px">
+			<div><h2 style="text-align: center;">Client AB</h2></div>
+			<hr>
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="#">Test</a></li>
+				<li><a href="#">Test</a></li>
+				<li><a href="#">Test</a></li>				
+			</ul>
+		</div>
 		<div class="col-md-9 pull-md-3" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin:10px 0px 30px 0px">
 			<div><h2 style="text-align: center;">Website</h2></div>
 			<div class="progress">
